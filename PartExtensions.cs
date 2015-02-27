6cde446c63869257a23753b05c58c5b461604ff0
Gabe Korgood <korgood@cooper.edu>
2015-02-26 23:36:06 -0500

    Source Code
    The plugin's source code files

---
+++ PartExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace In_Game_Clock.Extensions
+{
+    public static class PartExtensions
+    {
+        public static bool IsPrimary(this Part thisPart, List<Part> partsList, int moduleClassID)
+        {
+            foreach (Part part in partsList)
+            {
+                if (part.Modules.Contains(moduleClassID))
+                {
+                    if (part == thisPart)
+                        return true;
+                    else
+                        break;
+                }
+            }
+
+            return false;
+        }
+    }
+}
